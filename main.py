c8d00c750c0a9289e1ae1de0701d28820b6a9da7
aotondo <andres@otondo.cl>
2020-08-09 12:48:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import pyudev
 import threading
 import logging
-
-class USBDetector():
+import os
+import subprocess
+import time
+from os import system, name 
+class usbshredder():
     def __init__(self):
+        self.show_lcd('clear')
         thread = threading.Thread(target=self._work)
         thread.daemon = False
         thread.start()
- 
+    def shred(self, block):
+        self.show_lcd('clear')
+        self.show_lcd("Shredding /dev/" + block)
+        p = subprocess.check_output(["dd", "if=/dev/urandom", "of=/dev/" + block, "bs=4M", "status=progress"])
+    def show_lcd(self, lcdstr):
+        if lcdstr == "clear":
+            system('clear')
+        else: 
+            print(lcdstr)
     def _work(self):
         self.context = pyudev.Context()
         self.monitor = pyudev.Monitor.from_netlink(self.context)
         self.monitor.filter_by(subsystem='block')
-        print("READY")
+        self.show_lcd("READY")
         self.monitor.start()
         for device in iter(self.monitor.poll, None):
             if device.sys_path[-3:].isalpha():
                 if device.action == 'add':
-                    print(device.sys_path[-3:] + " plugged")
-                if device.action == 'remove':
-                    print(device.sys_path[-3:] + " removed")
+                    self.show_lcd('clear')
+                    self.show_lcd("USB block " + device.sys_path[-3:] + " plugged in")
+                    time.sleep(2)
+                    self.shred(device.sys_path[-3:])          
 
-a = USBDetector() 
+                if device.action == 'remove':
+                    self.show_lcd("\nUSB block " + device.sys_path[-3:] + " removed")
+                    p2 = subprocess.Popen(["killall", "dd"])
+                    #p.terminate()
+                    time.sleep(2)
+                    self.show_lcd('clear')
+                    self.show_lcd("READY")
+a = usbshredder()
