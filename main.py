25d283dcc82d192bc8f65e9408e198a299a1801a
aotondo <andres@otondo.cl>
2020-08-12 20:08:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ import logging
 import os
 import subprocess
 import time
-from os import system, name 
+from subprocess import Popen, PIPE
+from os import system, name
 class usbshredder():
     def __init__(self):
         self.show_lcd('clear')
         thread = threading.Thread(target=self._work)
         thread.daemon = False
         thread.start()
+    
     def shred(self, block):
         self.show_lcd('clear')
         self.show_lcd("Shredding /dev/" + block)
-        p = subprocess.Popen(["dd", "if=/dev/urandom", "of=/dev/" + block, "bs=4M", "status=progress"])
+        p = subprocess.Popen(["dd", "if=/dev/urandom", "of=/dev/" + block, "bs=4M", "status=progress"], bufsize=1, stdout=PIPE, shell=True).stdout
+        self.show_lcd(p[0])
+
     def show_lcd(self, lcdstr):
         if lcdstr == "clear":
             system('clear')
         else: 
             print(lcdstr)
+    
     def _work(self):
         self.context = pyudev.Context()
         self.monitor = pyudev.Monitor.from_netlink(self.context)
@@ -31,13 +36,14 @@ class usbshredder():
                 if device.action == 'add':
                     self.show_lcd('clear')
                     self.show_lcd("USB block " + device.sys_path[-3:] + " plugged in")
-                    time.sleep(2)
+                    time.sleep(1)
                     self.shred(device.sys_path[-3:])          
 
                 if device.action == 'remove':
-                    self.show_lcd("\nUSB block " + device.sys_path[-3:] + " removed")
-                    p2 = subprocess.Popen(["killall", "dd"])
-                    time.sleep(2)
+                    self.show_lcd('clear')
+                    self.show_lcd("USB block " + device.sys_path[-3:] + " removed")
+                    subprocess.Popen(["killall", "dd"], stdout=PIPE)
+                    time.sleep(1)
                     self.show_lcd('clear')
                     self.show_lcd("READY")
 a = usbshredder()
