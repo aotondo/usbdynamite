4b32ac88c8414f429c6e12e73d9ffae62a017c04
aotondo <andres@otondo.cl>
2020-12-07 20:18:01 -0300

    Update main.py
    LCD backlight forced

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Lcd_class():
   #LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
   #LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
 
-  #LCD_BACKLIGHT  = 0x08  # On
-  LCD_BACKLIGHT = 0x00  # Off
+  LCD_BACKLIGHT  = 0x08  # On
+  #LCD_BACKLIGHT = 0x00  # Off
 
   ENABLE = 0b00000100 # Enable bit
 
@@ -36,44 +36,54 @@ class Lcd_class():
   E_DELAY = 0.0005
   bus = smbus.SMBus(1) ## DESCOMENTAR PARA USAR CON LCD
 
-  def __init__(self):
-    self.lcd_byte(0x33,Lcd_class.LCD_CMD) # 110011 Initialise
-    self.lcd_byte(0x32,Lcd_class.LCD_CMD) # 110010 Initialise
-    self.lcd_byte(0x06,Lcd_class.LCD_CMD) # 000110 Cursor move direction
-    self.lcd_byte(0x0C,Lcd_class.LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
-    self.lcd_byte(0x28,Lcd_class.LCD_CMD) # 101000 Data length, number of lines, font size
-    self.lcd_byte(0x01,Lcd_class.LCD_CMD) # 000001 Clear display
-    time.sleep(Lcd_class.E_DELAY) 
+  def lcd_init(self):
+    self.lcd_byte(0x33,self.LCD_CMD) # 110011 Initialise
+    self.lcd_byte(0x32,self.LCD_CMD) # 110010 Initialise
+    self.lcd_byte(0x06,self.LCD_CMD) # 000110 Cursor move direction
+    self.lcd_byte(0x0C,self.LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+    self.lcd_byte(0x28,self.LCD_CMD) # 101000 Data length, number of lines, font size
+    self.lcd_byte(0x01,self.LCD_CMD) # 000001 Clear display
+    time.sleep(self.E_DELAY)
 
   def lcd_clear(self):
-    self.lcd_byte(0x01,Lcd_class.LCD_CMD)
+    self.lcd_byte(0x01,self.LCD_CMD)
   
   def lcd_byte(self, bits, mode):
-    bits_high = mode | (bits & 0xF0) | Lcd_class.LCD_BACKLIGHT
-    bits_low = mode | ((bits<<4) & 0xF0) | Lcd_class.LCD_BACKLIGHT
+    bits_high = mode | (bits & 0xF0) | 0x08
+    bits_low = mode | ((bits<<4) & 0xF0) | 0x08
 
     # High bits
-    Lcd_class.bus.write_byte(Lcd_class.I2C_ADDR, bits_high)
+    self.bus.write_byte(self.I2C_ADDR, bits_high)
     self.lcd_toggle_enable(bits_high)
 
     # Low bits
-    Lcd_class.bus.write_byte(Lcd_class.I2C_ADDR, bits_low)
+    self.bus.write_byte(self.I2C_ADDR, bits_low)
     self.lcd_toggle_enable(bits_low)
+    self.bus.write_byte(0x27, 0x08)
 
   def lcd_toggle_enable(self, bits):
     # Toggle enable
-    time.sleep(Lcd_class.E_DELAY)
-    self.bus.write_byte(Lcd_class.I2C_ADDR, (bits | Lcd_class.ENABLE))
-    time.sleep(Lcd_class.E_PULSE)
-    self.bus.write_byte(Lcd_class.I2C_ADDR,(bits & Lcd_class.ENABLE))
-    time.sleep(Lcd_class.E_DELAY)
+    time.sleep(self.E_DELAY)
+    self.bus.write_byte(self.I2C_ADDR, (bits | self.ENABLE))
+    time.sleep(self.E_PULSE)
+    self.bus.write_byte(self.I2C_ADDR,(bits & self.ENABLE))
+    time.sleep(self.E_DELAY)
 
+  #def lcd_string(self, message,line):
+    # Send string to display
+  #  message = message.ljust(Lcd_class.LCD_WIDTH," ")
+  #  self.lcd_byte(line, Lcd_class.LCD_CMD)
+  #  for i in range(Lcd_class.LCD_WIDTH):
+  #    self.lcd_byte(ord(message[i]),Lcd_class.LCD_CHR)
+  
   def lcd_string(self, message,line):
     # Send string to display
-    message = message.ljust(Lcd_class.LCD_WIDTH," ")
-    self.lcd_byte(line, Lcd_class.LCD_CMD)
-    for i in range(Lcd_class.LCD_WIDTH):
-      self.lcd_byte(ord(message[i]),Lcd_class.LCD_CHR)
+    message = message.ljust(16," ")
+    self.lcd_byte(line, self.LCD_CMD)
+    for i in range(16):
+      self.lcd_byte(ord(message[i]),self.LCD_CHR)
+      
+
 
 class Usbshredder():
     def __init__(self):
@@ -137,15 +147,17 @@ class Usbshredder():
               line = line + s
       
     def show_lcd(self, lcdstr, status = " "):
-        asdf = Lcd_class()  ## DESCOMENTAR PARA USAR CON LCD
-        if lcdstr == "clear":
-            system('clear')
-            asdf.lcd_clear()  ## DESCOMENTAR PARA USAR CON LCD
-        else: 
-            print(lcdstr)
-            print(status)
-            asdf.lcd_string(lcdstr, asdf.LCD_LINE_1)  ## DESCOMENTAR PARA USAR CON LCD
-            asdf.lcd_string(status, asdf.LCD_LINE_2)  ## DESCOMENTAR PARA USAR CON LCD
+      asdf = Lcd_class()  ## DESCOMENTAR PARA USAR CON LCD
+      #asdf.lcd_init()
+
+      if lcdstr == "clear":
+          system('clear')
+          asdf.lcd_clear()  ## DESCOMENTAR PARA USAR CON LCD
+      else: 
+          print(lcdstr)
+          print(status)
+          asdf.lcd_string(lcdstr, asdf.LCD_LINE_1)  ## DESCOMENTAR PARA USAR CON LCD
+          asdf.lcd_string(status, asdf.LCD_LINE_2)  ## DESCOMENTAR PARA USAR CON LCD
 
     def _work(self):
         global t0
